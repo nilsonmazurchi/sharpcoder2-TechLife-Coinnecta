0b5c22274aac1c541e2376cce5235b87aa277111
Matheus Panobianco <matheuspanobianco@hotmail.com>
2024-02-15 12:24:41 -0300

    "tela de login com inputs e botoes"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', pathMatch: 'full', redirectTo: 'login' },
+    { path: 'login', component: LoginComponent },
+    { path: '', component: LoginComponent }
+];
