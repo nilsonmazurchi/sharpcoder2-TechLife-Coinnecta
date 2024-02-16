72b6645081c4fae4093f45c70d2cf64cf725f56e
fhssouza <fhssouza@gmail.com>
2024-02-16 19:29:41 -0300

    Revert "Estilização dos componentes home, navbar e footer"
    This reverts commit 250d2883e1dbfd5762e90060e71ca2e3800276c0.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,14 +1,11 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 import {FooterComponent} from "../footer/footer.component";
 import {NavbarComponent} from "../navbar/navbar.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [
-    FooterComponent,
-    NavbarComponent,
-  ],
+  imports: [FooterComponent, NavbarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
-import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [
-    RouterLink
-  ],
+  imports: [],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
