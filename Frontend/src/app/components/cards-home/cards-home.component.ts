0b74339f72b3dbd18af06372002290846c7f2e64
fhssouza <fhssouza@gmail.com>
2024-02-17 15:10:20 -0300

    criação dos components carousel e cards

---
+++ Frontend/src/app/components/cards-home/cards-home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cards-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './cards-home.component.html',
+  styleUrl: './cards-home.component.css'
+})
+export class CardsHomeComponent {
+
+}
