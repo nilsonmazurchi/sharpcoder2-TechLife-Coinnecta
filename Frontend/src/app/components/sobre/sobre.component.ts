1c5680732e07d1eda7755a25aff503f8be145e11
nilsonmazurchi <nmazurchi@gmail.com>
2024-02-20 13:24:44 -0300

    Implementação mensagem personalisada

--- Frontend/src/app/components/sobre/sobre.component.ts
+++ Frontend/src/app/components/sobre/sobre.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../footer/footer.component';
 import { NavbarComponent } from '../navbar/navbar.component';
+import {RouterLink} from "@angular/router";
+
+
 
 @Component({
   selector: 'app-sobre',
   standalone: true,
   imports: [
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    RouterLink
   ],
   templateUrl: './sobre.component.html',
   styleUrl: './sobre.component.css'
