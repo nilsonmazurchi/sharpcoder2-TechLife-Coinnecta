449c071fa2dbaeccdf26e4efd9fe1a2cf36281c8
Bianca Santo <bianca.ces81@gmail.com>
2024-02-21 16:03:37 -0400

    Implantação de estilização na página de histórico

--- Frontend/src/app/pages/historico/historico.component.ts
+++ Frontend/src/app/pages/historico/historico.component.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { HistoricoNavComponent } from "../../components/historico/historico-nav/historico-nav.component";
 import { InfoComponent } from "../../components/historico/info/info.component";
 import { ExtratoComponent } from "../../components/historico/extrato/extrato.component";
+import { FooterComponent } from "../../components/footer/footer.component";
 
 @Component({
     selector: 'app-historico',
     standalone: true,
     templateUrl: './historico.component.html',
     styleUrl: './historico.component.css',
-    imports: [CommonModule, HistoricoNavComponent, InfoComponent, ExtratoComponent]
+    imports: [CommonModule, HistoricoNavComponent, InfoComponent, ExtratoComponent, FooterComponent]
 })
 export class HistoricoComponent {
 
