c6c2a935a6b16242a8f7d45325bd3c3b87507694
fhssouza <fhssouza@gmail.com>
2024-03-24 11:49:56 -0300

    login com jwt

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,19 +1,33 @@
-import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
-import { AuthService } from './servico/auth.service'; 
-@Injectable({
-  providedIn: 'root'
-})
-export class AuthGuard implements CanActivate {
+import {CanActivateFn, Router} from '@angular/router';
+import {AuthService} from "./servico/auth.service";
+import {inject} from "@angular/core";
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class AuthGuard implements CanActivate {
+//
+//   constructor(private authService: AuthService, private router: Router) {}
+//
+//   canActivate(): boolean {
+//     if (this.authService.isLoggedInUser()) {
+//       return true;
+//     } else {
+//       this.router.navigate(['/login']); // Redireciona para a página de login se o usuário não estiver autenticado
+//       return false;
+//     }
+//   }
+// }
 
-  constructor(private authService: AuthService, private router: Router) {}
+export const authGuard: CanActivateFn = (route, state) => {
 
-  canActivate(): boolean {
-    if (this.authService.isLoggedInUser()) {
-      return true;
-    } else {
-      this.router.navigate(['/login']); // Redireciona para a página de login se o usuário não estiver autenticado
-      return false;
-    }
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  if (authService.getToken()){
+    return true;
   }
+
+  router.navigate(['login']);
+  return false;
+
 }
